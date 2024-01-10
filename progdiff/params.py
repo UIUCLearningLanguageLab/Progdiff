b064dfeab686997285cb09bdb30d9206f5c726cc
Jon Willits <jwillits@illinois.edu>
2024-01-09 19:50:12 -0600

    rework train model

--- progdiff/params.py
+++ progdiff/params.py
@@ -11,36 +11,36 @@ param2requests = {
 }
 
 param2default = {
+    'device': 'mps',
+
     'corpus_path': 'input_data/childes',
     'category_file_path': 'input_data/categories.csv',
     'vocab_size': 4096,
     'window_size': None,
 
-    'device': 'mps',
-    'embedding_size': 64,
-    'hidden_layer_info_list': (('lstm', 256),),
+    'embedding_size': 0,
+    'hidden_layer_info_list': (('lstm', 512),),
     'weight_init': 0.0001,
-    'sequence_length': 3,
-
+    'sequence_length': 8,
     'criterion': 'cross_entropy',
 
     'num_epochs': 10,
     'optimizer': 'adagrad',
-    'learning_rate': 0.01,
-    'batch_size': 1,
+    'learning_rate': 0.001,
+    'batch_size': 64,
 
-    'evaluation_layer': 0,
+    'evaluation_layer': 1,
     'sequence_list': None,
 
-    'eval_freq': 10,
+    'eval_freq': 100,
 
     'run_cohyponym_task': True,
     'num_thresholds': 31,
 
-    'num_classifiers': 10,
+    'num_classifiers': 100,
     'run_classifier_task': True,
-    'classifier_hidden_sizes': [],
+    'classifier_hidden_sizes': (),
     'test_proportion': .1,
-    'classifier_epochs': 1,
-    'classifier_lr': .2
+    'classifier_epochs': 10,
+    'classifier_lr': .01
 }
--- progdiff/progdiff.py
+++ progdiff/progdiff.py
@@ -1,9 +1,11 @@
 import time
+import torch
 from distributional_models.corpora.childes import Childes
 from distributional_models.tasks.categories import Categories
 from distributional_models.tasks.cohyponym_task import CohyponymTask
 from distributional_models.tasks.classifier import classify
-from distributional_models.models.neural_network import NeuralNetwork
+from distributional_models.models.lstm import SimpleLSTM
+import torch.optim as optim
 
 
 def main():
@@ -28,8 +30,10 @@ def progdiff(param2val, run_location):
     the_corpus, missing_words = init_corpus(param2val['vocab_size'],
                                             the_categories.instance_list,
                                             param2val['corpus_path'])
+
     the_categories.remove_instances(missing_words)
     the_model = init_model(the_corpus,
+                           param2val['sequence_length'],
                            param2val['embedding_size'],
                            param2val['hidden_layer_info_list'],
                            param2val['weight_init'],
@@ -73,8 +77,14 @@ def create_corpus(corpus_path, language="eng", collection_name=None, age_range_t
     return the_corpus
 
 
-def init_model(corpus, embedding_size, hidden_layer_info_list, weight_init, device, criterion):
-    model = NeuralNetwork(corpus, embedding_size, hidden_layer_info_list, weight_init, criterion, device=device)
+def init_model(corpus, block_size, embedding_size, hidden_layer_info_list, weight_init, device, criterion):
+    model = SimpleLSTM(corpus,
+                       block_size,
+                       embedding_size,
+                       hidden_layer_info_list,
+                       weight_init,
+                       criterion,
+                       device=device)
     return model
 
 
@@ -87,85 +97,150 @@ def cohyponym_task(the_categories, num_thresholds):
 
 
 def classifier_task(the_categories, classifier_hidden_sizes, test_proportion, classifier_epochs,
-                    classifier_lr):
+                    classifier_lr, num_classifiers):
+
     start_time = time.time()
-    train_df, test_df = classify(the_categories, classifier_hidden_sizes, test_proportion=test_proportion,
-                                 num_epochs=classifier_epochs, learning_rate=classifier_lr)
-    train_acc = train_df['correct'].mean()
-    test_acc = test_df['correct'].mean()
+
+    train_0_sum = 0
+    train_1_sum = 0
+    train_final_sum = 0
+    test_0_sum = 0
+    test_1_sum = 0
+    test_final_sum = 0
+
+    for k in range(num_classifiers):
+        the_categories.create_xy_lists()
+        start_time = time.time()
+        train_df_list, test_df_list = classify(the_categories, classifier_hidden_sizes, test_proportion=test_proportion,
+                                               num_epochs=classifier_epochs, learning_rate=classifier_lr)
+        train_acc_0 = train_df_list[0]['correct'].mean()
+        train_acc_1 = train_df_list[1]['correct'].mean()
+        train_acc_final = train_df_list[-1]['correct'].mean()
+        test_acc_0 = test_df_list[0]['correct'].mean()
+        test_acc_1 = test_df_list[1]['correct'].mean()
+        test_acc_final = test_df_list[-1]['correct'].mean()
+
+        train_0_sum += train_acc_0
+        train_1_sum += train_acc_1
+        train_final_sum += train_acc_final
+        test_0_sum += test_acc_0
+        test_1_sum += test_acc_1
+        test_final_sum += test_acc_final
+
+    train_0_mean = train_0_sum / num_classifiers
+    train_1_mean = train_1_sum / num_classifiers
+    train_final_mean = train_final_sum / num_classifiers
+    test_0_mean = test_0_sum / num_classifiers
+    test_1_mean = test_1_sum / num_classifiers
+    test_final_mean = test_final_sum / num_classifiers
+
     took = time.time() - start_time
-    return train_df, test_df, train_acc, test_acc, took
+
+    return train_0_mean, train_1_mean, train_final_mean, test_0_mean, test_1_mean, test_final_mean, took
+
+
+def prepare_batches(corpus, model, train_params):
+    doc_index_list = corpus.flatten_corpus_lists(corpus.document_list[j])
+
+    corpus.x_list, corpus.y_list = corpus.create_index_list(doc_index_list,
+                                                            corpus.vocab_index_dict,
+                                                            corpus.unknown_token,
+                                                            window_size=train_params['window_size'])
+    index_list = corpus.x_list + [corpus.y_list[-1]]
+
+    sequence_list = corpus.create_sequence_lists(index_list, train_params['sequence_length'], 0)
+
+    x_batches, y_batches = corpus.create_batches(sequence_list, train_params['batch_size'],
+                                                 train_params['sequence_length'], 0)
+
+    x_batches = [torch.tensor(x_batch, dtype=torch.long).to(model.device) for x_batch in x_batches]
+    y_batches = [torch.tensor(y_batch, dtype=torch.long).to(model.device) for y_batch in y_batches]
+
+    return x_batches, y_batches
+
+
+def evaluate_model(i, j, model, the_categories, corpus, train_params, training_took, loss_sum, tokens_sum):
+    loss_mean = loss_sum / tokens_sum
+
+    output_string = f"{i}-{j}-{tokens_sum:<9}  loss:{loss_mean:<7.4f}"
+
+    weight_matrix = model.get_weights(train_params['evaluation_layer'])
+    the_categories.set_instance_feature_matrix(weight_matrix, corpus.vocab_index_dict)
+
+    if train_params['run_cohyponym_task']:
+        the_cohyponym_task, mean_ba, ba_took = cohyponym_task(the_categories,
+                                                              train_params['num_thresholds'])
+        output_string += f"  BA:{mean_ba:0.3f}"
+    else:
+        ba_took = 0
+
+    if train_params['run_classifier_task']:
+        train_0_mean, \
+            train_1_mean, \
+            train_final_mean, \
+            test_0_mean, \
+            test_1_mean, \
+            test_final_mean, \
+            classifier_took = classifier_task(the_categories,
+                                              train_params['classifier_hidden_sizes'],
+                                              train_params['test_proportion'],
+                                              train_params['classifier_epochs'],
+                                              train_params['classifier_lr'],
+                                              train_params['num_classifiers'])
+        output_string += f"  Classify0:{train_0_mean:0.3f}-{test_0_mean:0.3f}"
+        output_string += f"  Classify1:{train_1_mean:0.3f}-{test_1_mean:0.3f}"
+        output_string += f"  ClassifyN:{train_final_mean:0.3f}-{test_final_mean:0.3f}"
+
+    else:
+        classifier_took = 0
+
+    output_string += f"  Took:{training_took:0.2f}-{ba_took:0.2f}-{classifier_took:0.2f}"
+    print(output_string)
 
 
 def train_model(corpus, model, the_categories, train_params):
 
     performance_dict = {}
 
+    model.train()
+
+    optimizer = optim.Adam(model.parameters(), lr=train_params['learning_rate'])
+    criterion = torch.nn.CrossEntropyLoss()
+
     for i in range(train_params['num_epochs']):
-        took_sum = 0
-        tokens = 0
-        perplexity_sum = 0
-        n = 0
+        loss_sum = 0
+        tokens_sum = 0
+
         for j in range(len(corpus.document_list)):
 
-            doc_sequence_list = corpus.flatten_corpus_lists(corpus.document_list[j])
-            corpus.x_list, corpus.y_list = corpus.create_sequence_list(doc_sequence_list,
-                                                                       corpus.vocab_index_dict,
-                                                                       corpus.unknown_token,
-                                                                       window_size=train_params['window_size'])
-            took, perplexity = model.train_sequence(corpus.x_list,
-                                                    corpus.y_list,
-                                                    train_params['optimizer'],
-                                                    train_params['learning_rate'],
-                                                    batch_size=train_params['batch_size'],
-                                                    sequence_length=train_params['sequence_length'])
-            tokens += len(doc_sequence_list)
-            took_sum += took
-            perplexity_sum += perplexity*len(doc_sequence_list)
-            n += 1
+            start_time = time.time()
+            x_batches, y_batches = prepare_batches(corpus, model, train_params)
+
+            hidden = (torch.zeros(1, train_params['batch_size'], model.hidden_size).to(model.device),
+                      torch.zeros(1, train_params['batch_size'], model.hidden_size).to(model.device))
+
+            for x_batch, y_batch in zip(x_batches, y_batches):
+
+                optimizer.zero_grad()
+                output, hidden = model(x_batch, hidden)
+                hidden = (hidden[0].detach(), hidden[1].detach())
+
+                loss = criterion(output.view(-1, corpus.vocab_size), y_batch.view(-1))
+                mask = y_batch.view(-1) != 0
+                loss = (loss * mask).mean()
+                loss.backward()
+                optimizer.step()
+
+                loss_sum += loss.item()
+                tokens_sum += train_params['batch_size']
+
+            training_took = time.time() - start_time
 
             if j % train_params['eval_freq'] == 0:
-                # model training output
-                perplexity_mean = perplexity_sum/tokens
-                output_string = f"{i}-{j}-{tokens:<9}  took:{took_sum:2.2f}  perp:{perplexity_mean:<7.2f}"
-                took_sum = 0
-                tokens = 0
-                perplexity_sum = 0
-                n = 0
+                evaluate_model(i, j, model, the_categories, corpus, train_params, training_took, loss_sum, tokens_sum)
+                loss_sum = 0
+                tokens_sum = 0
 
-                weight_matrix = model.get_weights(train_params['evaluation_layer'])
-                the_categories.set_instance_feature_matrix(weight_matrix, corpus.vocab_index_dict)
-
-                if train_params['run_cohyponym_task']:
-                    the_cohyponym_task, mean_ba, ba_took = cohyponym_task(the_categories,
-                                                                          train_params['num_thresholds'])
-                    output_string += f"  BA:{mean_ba:0.3f}"
-
-                if train_params['run_classifier_task']:
-
-                    train_sum = 0
-                    test_sum = 0
-
-                    for k in range(train_params['num_classifiers']):
-                        the_categories.create_xy_lists()
-                        train_df, \
-                            test_df, \
-                            train_acc, \
-                            test_acc, \
-                            classify_took = classifier_task(the_categories,
-                                                            train_params['classifier_hidden_sizes'],
-                                                            train_params['test_proportion'],
-                                                            train_params['classifier_epochs'],
-                                                            train_params['classifier_lr'])
-                        train_sum += train_acc
-                        test_sum += test_acc
-
-                    if train_params['num_classifiers'] > 0:
-                        train_mean = train_sum / train_params['num_classifiers']
-                        test_mean = test_sum / train_params['num_classifiers']
-                        output_string += f"  Classify:{train_mean:0.3f}-{test_mean:0.3f}"
-
-                print(output_string)
     return performance_dict
 
 
