7327f52ddd20843bb30ef1936c89f50683a0f97e
Jon Willits <jwillits@illinois.edu>
2024-01-14 08:06:10 -0600

    simplify main

--- progdiff/params.py
+++ progdiff/params.py
@@ -11,38 +11,75 @@ param2requests = {
 }
 
 param2default = {
+    'random_seed': None,
     'device': 'mps',
 
     'corpus_path': 'input_data/childes',
     'category_file_path': 'input_data/categories.csv',
+    'num_document_batches': 16,
+    'document_order': 'age_ordered',
     'vocab_size': 4096,
-    'window_size': None,
 
-    'embedding_size': 0,
-    'hidden_layer_info_list': (('lstm', 512),),
+    # Model Params
+    'model_type': 'lstm',
     'weight_init': 0.0001,
+    'save_path': 'models/',
+    'save_freq': 100,
     'sequence_length': 8,
+
+    # SRN & LSTM Params
+    'rnn_embedding_size': 0,
+    'rnn_hidden_size': 512,
+
+    # W2V Params
+    'w2v_embedding_size': 0,
+    'w2v_hidden_size': 256,
+    'corpus_window_size': 16,
+
+    # Transformer params
+    'transformer_embedding_size': 64,
+    'transformer_num_heads': 8,
+    'transformer_attention_size': 32,
+    # 'transformer_num_layers': 3,
+    'transformer_hidden_size': 128,
+    'transformer_target_output': 'single_y',
+
+    # Training Params
+    'num_epochs': 5,
     'criterion': 'cross_entropy',
-
-    'num_epochs': 10,
     'optimizer': 'adagrad',
-    'learning_rate': 0.001,
+    'learning_rate': 0.01,
     'batch_size': 64,
+    'dropout_rate': 0.0,
+    'l1_lambda': 0.0,
+    'weight_decay': 0.0,
 
-    'evaluation_layer': 'lstm',
+    # evaluation params
+    'eval_freq': 1,
+    'evaluation_layer': 'output',
     'sequence_list': None,
 
-    'eval_freq': 100,
-    'save_freq': 100,
-    'save_path': 'models/',
-
+    # cohyponym task params
     'run_cohyponym_task': True,
-    'num_thresholds': 31,
+    'cohyponym_similarity_metric': 'correlation',
+    'cohyponym_num_thresholds': 51,
+    'cohyponym_only_best_thresholds': True,
 
-    'num_classifiers': 100,
-    'run_classifier_task': True,
+    # classifier task params
+    'run_classifier_task': False,
+    'num_classifiers': 1,
     'classifier_hidden_sizes': (),
-    'test_proportion': .1,
-    'classifier_epochs': 10,
-    'classifier_lr': .01
+    'classifier_num_folds': 10,
+    'classifier_num_epochs': 10,
+    'classifier_learning_rate': .05,
+    'classifier_batch_size': 64,
+    'classifier_criterion': 'cross_entropy',
+    'classifier_optimizer': 'adam',
+    'classifier_device': 'mps',
+
+    # generate sequence task params
+    'generate_sequence': True,
+    'prime_token_list': ('look', 'at'),
+    'generate_sequence_length': 10,
+    'generate_temperature': 1.0
 }
--- progdiff/progdiff.py
+++ progdiff/progdiff.py
@@ -1,285 +1,89 @@
-import time
-import os
-from datetime import datetime
-import torch
 from distributional_models.corpora.childes import Childes
 from distributional_models.tasks.categories import Categories
-from distributional_models.tasks.cohyponym_task import CohyponymTask
-from distributional_models.tasks.classifier import classify
-from distributional_models.models.srn import SRN
-from distributional_models.models.lstm import LSTM
-from distributional_models.models.mlp import MLP
+from distributional_models.scripts.create_model import create_model
+from distributional_models.scripts.evaluate_model import evaluate_model
 
 
 def main():
     import params
-    progdiff(params.param2default, "local")
+    run_progdiff(params.param2default, "local")
 
 
-def progdiff(param2val, run_location):
-
+def run_progdiff(param2val, run_location):
     param2val = fix_paths(param2val, run_location)
 
-    the_categories = init_categories(param2val['category_file_path'])
-    the_corpus, missing_words = init_corpus(param2val['vocab_size'],
-                                            the_categories.instance_list,
-                                            param2val['corpus_path'])
+    the_categories = Categories()
+    the_categories.create_from_category_file(param2val['category_file_path'])
 
+    the_corpus, missing_words = init_corpus(param2val,
+                                            the_categories.instance_list)
     the_categories.remove_instances(missing_words)
 
-    prep_output(param2val)
-
-    the_model = init_model(the_corpus,
-                           param2val['sequence_length'],
-                           param2val['embedding_size'],
-                           param2val['hidden_layer_info_list'],
-                           param2val['weight_init'],
-                           param2val['device'])
+    the_model = create_model(the_corpus.vocab_size, param2val)
 
     performance_dict = train_model(the_corpus, the_model, the_categories, param2val)
-
     return performance_dict
 
 
 def fix_paths(train_params, run_location):
-
     if run_location == 'local':
         train_params['corpus_path'] = "../" + train_params['corpus_path']  # "input_data/childes"
         train_params['category_file_path'] = "../" + train_params['category_file_path']  # "input_data/categories"
-        train_params['save_path'] = "../" + train_params['save_path']  # "models/"
-
-    elif run_location == 'ludwig_local':
-        pass
-
     elif run_location == 'ludwig_cluster':
         train_params['corpus_path'] = "/media/ludwig_data/Progdiff/" + train_params['corpus_path']
         train_params['category_file_path'] = "/media/ludwig_data/Progdiff/" + train_params['category_file_path']
         # TODO fix save_path for ludwig so it ends up in the same runs folder
-
     else:
         raise ValueError(f"Unrecognized run location {run_location}")
-
-    train_params['save_path'] += train_params['hidden_layer_info_list'][0][0]
-
-    train_params['save_path'] += f"_{train_params['vocab_size']}"
-    train_params['save_path'] += f"_{train_params['embedding_size']}"
-    train_params['save_path'] += f"_{train_params['hidden_layer_info_list'][0][1]}"
-
-    now = datetime.now()
-    date_time_string = now.strftime("%Y%m%d_%H%M%S")
-    train_params['save_path'] += f"_{date_time_string}"
 
     return train_params
 
 
-def prep_output(train_params):
-    os.mkdir(train_params['save_path'])
-
-
-def init_categories(category_file_path):
-    the_categories = Categories()
-    the_categories.create_from_category_file(category_file_path)
-    return the_categories
-
-
-def init_corpus(vocab_size, include_list, corpus_path):
+def init_corpus(params, include_list):
     # TODO this can be improved to catch specific exceptions, like is the file there, and the error that will occur
     # if you loaded a childes instance created under a different path
     try:
-        the_corpus = Childes.load_from_file(corpus_path+'.pkl')
+        the_corpus = Childes.load_from_file(params['corpus_path']+'.pkl')
     except:
-        the_corpus = create_corpus(corpus_path+".csv")
-        the_corpus.save_to_pkl_file(corpus_path)
-    missing_words = the_corpus.create_vocab(vocab_size=vocab_size, include_list=include_list, include_unknown=True)
+        the_corpus = Childes()
+        the_corpus.get_documents_from_childes_db_file(input_path=params['corpus_path'],
+                                                      language='eng',
+                                                      collection_name=None,
+                                                      age_range_tuple=(0,1000),
+                                                      sex_list=None,
+                                                      add_punctuation=True,
+                                                      exclude_target_child=True,
+                                                      num_documents=0)
+        the_corpus.save_to_pkl_file(params['corpus_path'])
+    missing_words = the_corpus.create_vocab(vocab_size=params['vocab_size'],
+                                            include_list=include_list,
+                                            include_unknown=True)
+    the_corpus.batch_docs_by_age(params['num_document_batches'], params['document_order'])
     return the_corpus, missing_words
 
 
-def create_corpus(corpus_path, language="eng", collection_name=None, age_range_tuple=(0, 1000),
-                  sex_list=None, add_punctuation=True, exclude_target_child=True, num_documents=0):
-    the_corpus = Childes()
-    the_corpus.get_documents_from_childes_db_file(input_path=corpus_path,
-                                                  language=language,
-                                                  collection_name=collection_name,
-                                                  age_range_tuple=age_range_tuple,
-                                                  sex_list=sex_list,
-                                                  add_punctuation=add_punctuation,
-                                                  exclude_target_child=exclude_target_child,
-                                                  num_documents=num_documents)
-    return the_corpus
-
-
-def init_model(corpus, block_size, embedding_size, hidden_layer_info_list, weight_init, device):
-    if hidden_layer_info_list[0][0] == 'lstm':
-        hidden_size = hidden_layer_info_list[0][1]
-        model = LSTM(corpus, embedding_size, hidden_size, weight_init, device)
-    elif hidden_layer_info_list[0][0] == 'srn':
-        hidden_size = hidden_layer_info_list[0][1]
-        model = SRN(corpus, embedding_size, hidden_size, weight_init, device)
-    elif hidden_layer_info_list[0][0] == 'mlp':
-        hidden_size = hidden_layer_info_list[0][1]
-        model = MLP(corpus, embedding_size, hidden_size, weight_init, device)
-    else:
-        raise ValueError(f"Unrecognized model type {hidden_layer_info_list[0][0]}")
-    return model
-
-
-def cohyponym_task(the_categories, num_thresholds):
-    start_time = time.time()
-    the_cohyponym_task = CohyponymTask(the_categories, num_thresholds=num_thresholds)
-    mean_ba = the_cohyponym_task.run_cohyponym_task()
-    took = time.time() - start_time
-    return the_cohyponym_task, mean_ba, took
-
-
-def classifier_task(the_categories, classifier_hidden_sizes, test_proportion, classifier_epochs,
-                    classifier_lr, num_classifiers):
-
-    start_time = time.time()
-
-    train_0_sum = 0
-    train_1_sum = 0
-    train_final_sum = 0
-    test_0_sum = 0
-    test_1_sum = 0
-    test_final_sum = 0
-
-    for k in range(num_classifiers):
-        the_categories.create_xy_lists()
-        start_time = time.time()
-        train_df_list, test_df_list = classify(the_categories, classifier_hidden_sizes, test_proportion=test_proportion,
-                                               num_epochs=classifier_epochs, learning_rate=classifier_lr)
-        train_acc_0 = train_df_list[0]['correct'].mean()
-        train_acc_1 = train_df_list[1]['correct'].mean()
-        train_acc_final = train_df_list[-1]['correct'].mean()
-        test_acc_0 = test_df_list[0]['correct'].mean()
-        test_acc_1 = test_df_list[1]['correct'].mean()
-        test_acc_final = test_df_list[-1]['correct'].mean()
-
-        train_0_sum += train_acc_0
-        train_1_sum += train_acc_1
-        train_final_sum += train_acc_final
-        test_0_sum += test_acc_0
-        test_1_sum += test_acc_1
-        test_final_sum += test_acc_final
-
-    train_0_mean = train_0_sum / num_classifiers
-    train_1_mean = train_1_sum / num_classifiers
-    train_final_mean = train_final_sum / num_classifiers
-    test_0_mean = test_0_sum / num_classifiers
-    test_1_mean = test_1_sum / num_classifiers
-    test_final_mean = test_final_sum / num_classifiers
-
-    took = time.time() - start_time
-
-    return train_0_mean, train_1_mean, train_final_mean, test_0_mean, test_1_mean, test_final_mean, took
-
-
-def prepare_batches(document_list, corpus, model, train_params):
-    doc_index_list = corpus.flatten_corpus_lists(document_list)
-
-    corpus.x_list, corpus.y_list, corpus.index_list = corpus.create_index_list(doc_index_list,
-                                                                               corpus.vocab_index_dict,
-                                                                               corpus.unknown_token,
-                                                                               window_size=train_params['window_size'])
-    index_list = corpus.x_list + [corpus.y_list[-1]]
-
-    sequence_list = corpus.create_sequence_lists(index_list, train_params['sequence_length'], 0)
-
-    x_batches, y_batches, y_window_batches = corpus.create_batches(sequence_list, train_params['batch_size'],
-                                                 train_params['sequence_length'], 0)
-
-    x_batches = [torch.tensor(x_batch, dtype=torch.long).to(model.device) for x_batch in x_batches]
-    y_batches = [torch.tensor(y_batch, dtype=torch.long).to(model.device) for y_batch in y_batches]
-
-    return x_batches, y_batches, y_window_batches
-
-
-def evaluate_model(i, j, model, the_categories, corpus, train_params, training_took, loss_sum, tokens_sum):
-    loss_mean = loss_sum / tokens_sum
-
-    output_string = f"{i}-{j}-{tokens_sum:<9}  loss:{loss_mean:<7.4f}"
-
-    weight_matrix = model.get_weights(train_params['evaluation_layer'])
-    the_categories.set_instance_feature_matrix(weight_matrix, corpus.vocab_index_dict)
-
-    if train_params['run_cohyponym_task']:
-        the_cohyponym_task, mean_ba, ba_took = cohyponym_task(the_categories,
-                                                              train_params['num_thresholds'])
-        output_string += f"  BA:{mean_ba:0.3f}"
-    else:
-        ba_took = 0
-
-    if train_params['run_classifier_task']:
-        train_0_mean, \
-            train_1_mean, \
-            train_final_mean, \
-            test_0_mean, \
-            test_1_mean, \
-            test_final_mean, \
-            classifier_took = classifier_task(the_categories,
-                                              train_params['classifier_hidden_sizes'],
-                                              train_params['test_proportion'],
-                                              train_params['classifier_epochs'],
-                                              train_params['classifier_lr'],
-                                              train_params['num_classifiers'])
-        output_string += f"  Classify0:{train_0_mean:0.3f}-{test_0_mean:0.3f}"
-        output_string += f"  Classify1:{train_1_mean:0.3f}-{test_1_mean:0.3f}"
-        output_string += f"  ClassifyN:{train_final_mean:0.3f}-{test_final_mean:0.3f}"
-
-    else:
-        classifier_took = 0
-
-    output_string += f"  Took:{training_took:0.2f}-{ba_took:0.2f}-{classifier_took:0.2f}"
-    print(output_string)
-
-
 def train_model(corpus, model, the_categories, train_params):
-
     performance_dict = {}
-    model.train()
+    took_sum = 0
 
-    model.set_optimizer(train_params['optimizer'], train_params['learning_rate'])
-    model.set_criterion(train_params['criterion'])
+    for j in range(len(corpus.document_list)):
+        print(f"Training corpus chunk {j+1}/{len(corpus.document_list)} for {train_params['num_epochs']} epochs")
+        for i in range(train_params['num_epochs']):
+            sequence = corpus.document_list[j]
 
-    for i in range(train_params['num_epochs']):
-        loss_sum = 0
-        tokens_sum = 0
-
-        for j in range(len(corpus.document_list)):
-
-            start_time = time.time()
-            x_batches, y_batches, y_window_batches = prepare_batches(corpus.document_list[j], corpus, model, train_params)
-            model.init_network(train_params['batch_size'], train_params['sequence_length'])
-
-            for x_batch, y_batch in zip(x_batches, y_batches):
-
-                model.optimizer.zero_grad()
-                output = model(x_batch)
-
-                if 'lstm' in model.hidden_dict:
-                    model.hidden_dict['lstm'] = (model.hidden_dict['lstm'][0].detach(),
-                                                 model.hidden_dict['lstm'][1].detach())
-                elif 'srn' in model.hidden_dict:
-                    model.hidden_dict['srn'] = model.hidden_dict['srn'].detach()
-
-                loss = model.criterion(output.view(-1, corpus.vocab_size), y_batch.view(-1))
-                mask = y_batch.view(-1) != 0
-                loss = (loss * mask).mean()
-                loss.backward()
-                model.optimizer.step()
-
-                loss_sum += loss.item()
-                tokens_sum += train_params['batch_size']
-
-            training_took = time.time() - start_time
+            loss_mean, took = model.train_sequence(corpus, sequence, train_params)
+            took_sum += took
 
             if j % train_params['eval_freq'] == 0:
-                evaluate_model(i, j, model, the_categories, corpus, train_params, training_took, loss_sum, tokens_sum)
-                loss_sum = 0
-                tokens_sum = 0
+                took_mean = took_sum / train_params['eval_freq']
+                took_sum = 0
+                output_string = evaluate_model(f"d{j}_e{i}", model, the_categories, corpus, train_params, took_mean,
+                                               loss_mean)
+                print(output_string)
 
             if j % train_params['save_freq'] == 0:
-                model.save_model(train_params['save_path']+f"/e{i}_d{j}.pth")
+                file_name = f"d{j}_e{i}.pth"
+                model.save_model(train_params['save_path'], file_name)
 
     return performance_dict
 
